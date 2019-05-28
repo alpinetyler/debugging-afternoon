e54b2440179a947eb4b9189a5a64359f422b85ae
TylerT <alpinetyler@gmail.com>
2019-05-28 14:35:45 -0600

    complete

--- black-diamond/src/App.js
+++ black-diamond/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import axios from "axios";
 import StoreFront from "./Components/StoreFront/StoreFront";
 import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
 import NavBar from "./Components/NavBar/NavBar";
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
@@ -12,6 +12,9 @@ class App extends Component {
       cart: [],
       showCart: false
     };
+    this.addToCart = this.addToCart.bind(this);
+    this.removeFromCart = this.removeFromCart.bind(this);
+    this.navigate = this.navigate.bind(this);
   }
   componentDidMount() {
     axios
@@ -36,9 +39,9 @@ class App extends Component {
   }
   navigate(location) {
     if (location === "cart") {
-      this.state.showCart = true;
+      this.setState ({showCart: true});
     } else {
-      this.state.showCart = false;
+      this.setState ({showCart: false});
     }
   }
   render() {
@@ -48,7 +51,7 @@ class App extends Component {
         <NavBar navigate={this.navigate} />
         <div className="main-container">
           {showCart ? (
-            <ShoppingCart cart={cart} removeFromCart={this.removeFromCart} />
+            <ShoppingCart cart={this.state.cart} removeFromCart={this.removeFromCart} />
           ) : (
             <StoreFront products={products} addToCart={this.addToCart} />
           )}
